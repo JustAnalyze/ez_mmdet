82f2eca18ae0707c1aac704f240f86c8b1832f54
Justin Nacu <engr.nacu@gmail.com>
2026-01-30 17:12:44 +0800

    chore: Globally suppress noisy MMLab and PyTorch warnings
    Added global warning filters to the package __init__.py to silence redundant LocalVisBackend, meshgrid, and out-of-bounds warnings from underlying libraries, improving the developer experience during direct script execution and testing.

--- src/ez_openmmlab/__init__.py
+++ src/ez_openmmlab/__init__.py
@@ -1,3 +1,15 @@
+import warnings
+import logging
+
+# Suppress noisy MMLab and PyTorch warnings globally
+warnings.filterwarnings("ignore", message=".*LocalVisBackend")
+warnings.filterwarnings("ignore", message=".*meshgrid")
+warnings.filterwarnings("ignore", message=".*bbox is out of bounds")
+warnings.filterwarnings("ignore", message=".*polygon is out of bounds")
+
+# Suppress mmengine log warnings
+logging.getLogger("mmengine").setLevel(logging.ERROR)
+
 from .models.rtmdet import RTMDet
 from .core.base import EZMMLab
 from .core.detection import EZMMDetector
